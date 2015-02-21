3eedb2fdd525d3e6dc7c3850513bba038779010a
Lucas <lucas@agtlucas.com>
2015-02-20 23:57:07 -0200

    Updates

--- syntax.playground/section-1.swift
+++ syntax.playground/section-1.swift
@@ -1,4 +1,5 @@
 // Playground - noun: a place where people can play
+// http://cdn1.raywenderlich.com/wp-content/uploads/2014/06/RW-Swift-Cheatsheet-0_5.pdf
 
 import UIKit
 
@@ -7,3 +8,23 @@ var str = "Hello, playground"
 
 // constant
 let yo = "Hey!"
+
+// Class
+class SomeClass : optionalSuperClass, optionalProtocol1, optionalProtocol2 {
+
+    // Properties
+    var myProperty:String
+    var myOptionalProperty:String?
+
+    // Only need override if sublassing
+//    override init() {
+//        myProperty = "Foo"
+//    }
+
+    init() {
+        myProperty = "Foo"
+    }
+
+    // More methods
+
+}
